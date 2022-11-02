c032755f229d3ed4fb853577f651434c113efec9
swfz <sawafuji.09@gmail.com>
2022-11-02 19:32:08 +0900

    go: json Unmarshal

---
+++ go/json.go
@@ -0,0 +1,36 @@
+package main
+
+import(
+	"fmt"
+	"encoding/json"
+)
+
+type Sample struct {
+	Hoge int `json:"hoge"`
+	Fuga string `json:"fuga"`
+}
+
+func mapJson() {
+	str := `{"hoge":1,"fuga":"piyo"}`
+	var data Sample
+	if err := json.Unmarshal([]byte(str), &data); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%#v\n", data)
+}
+
+func sliceJson() {
+	str := `[{"hoge":1,"fuga":"piyo"},{"hoge":2,"fuga":"piyopiyo"}]`
+	var data []Sample
+	if err := json.Unmarshal([]byte(str), &data); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%#v\n", data)
+}
+
+func main() {
+	mapJson()
+	sliceJson()
+}
