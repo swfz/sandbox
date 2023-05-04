d478ba28034e708febc6533c0a179060489f6fe0
swfz <sawafuji.09@gmail.com>
2023-05-05 05:36:41 +0900

    stream

--- python/gpt.py
+++ python/gpt.py
@@ -1,10 +1,15 @@
 import openai
 
-res = openai.ChatCompletion.create(
+completion = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
             {"role": "user", "content": "プレゼン資料作成が終わりません！"}
-        ]
+        ],
+        stream=True
     )
 
-print(res.choices[0]["message"]["content"].strip())
+# print(res.choices[0]["message"]["content"].strip())
+
+for chunk in completion:
+    next = chunk["choices"][0]["delta"].get("content", "")
+    print(next, end="")
