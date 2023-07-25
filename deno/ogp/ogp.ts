4677d9f2c29fdf6c9f171999a21b804e123db27e
swfz <sawafuji.09@gmail.com>
2023-07-26 05:56:40 +0900

    feat: ターミナル風

--- deno/ogp/ogp.ts
+++ deno/ogp/ogp.ts
@@ -1,44 +1,75 @@
 import { createCanvas } from 'https://deno.land/x/canvas/mod.ts';
-import { serve } from "https://deno.land/std@0.194.0/http/server.ts";
 
 const port = 8080;
 
+const renderTriangle = (ctx: CanvasRenderingContext2D, x: number, y: number, color: string) => {
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(x + 40, y + 40);
+  ctx.lineTo(x, y + 80);
+  ctx.lineTo(x, y);
+  ctx.fill();
+}
+
+const renderPrefix = (ctx: CanvasRenderingContext2D, x: number, y: number) => {
+  ctx.fillStyle = '#efb24a';
+  ctx.fillText('$', x, y);
+};
+
 const handler = async (request: Request): Promise<Response> => {
   const url = new URL(request.url);
-  const title = url.searchParams.get("title") ?? null;
+  const title = url.searchParams.get("title") ?? 'No Title';
   const tagParam = url.searchParams.get("tags") ?? null;
 
   const tags = tagParam ? tagParam.split(',') : [];
 
   console.log(title);
-  const canvas = createCanvas(1200, 630);
+  const [width, height] = [1200, 630];
+
+  const canvas = createCanvas(width, height);
   const ctx = canvas.getContext("2d");
 
-  ctx.fillStyle = '#000033';
+  ctx.fillStyle = '#2f3e50';
   // ctx.fillStyle = '#666';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   // const font = await Deno.readFile('/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc');
   const font = await Deno.readFile('./NotoSansCJK-Regular.ttc');
   canvas.loadFont(font, {
-    family: 'hoge',
+    family: 'notosans',
   });
-  ctx.font = "70pt hoge";
+  ctx.font = "50pt notosans";
 
   // ctx.fillStyle = '#222222';
   // ctx.fillRect(0,0,1200,50);
   // ctx.fillRect(0,580,1200,630);
 
-  // TODO: タイトル強調
   // TODO: タイトル 改行判定
-  // TODO: ターミナル風
   // TODO: 色再度見る
-  // TODO: 同じ処理をどうしよう
-  ctx.fillStyle = '#AAAAAA';
-  ctx.fillText('> swfz > TIL >', 10, 100);
 
-  ctx.fillStyle = '#FCAF17';
-  ctx.fillText('$', 10, 200);
+  const terminalHeaders = ['swfz', 'til'];
+  const colors = ['#6797e8', '#a4e083']
+
+  const startPositions = terminalHeaders.reduce((acc, text) => {
+    const measured = ctx.measureText(text);
+    const x = acc.at(-1) + measured.width;
+
+    return [...acc, x];
+  }, [0]);
+
+  console.log(startPositions);
+
+  terminalHeaders.forEach((text, i) => {
+    ctx.fillStyle = colors[i];
+    ctx.fillRect(startPositions[i], 30, ctx.measureText(text).width, 80);
+    ctx.fillStyle = '#EFEFEF';
+    ctx.fillText(text, startPositions[i] + 50, 90);
+  });
+  terminalHeaders.forEach((text, i) => {
+    renderTriangle(ctx, startPositions[i+1], 30, colors[i])
+  });
+  renderPrefix(ctx, 10, 200);
+
   ctx.fillStyle = '#AAAAAA';
   ctx.fillText('title', 80, 200);
   ctx.fillStyle = '#FFFFFF';
@@ -46,15 +77,16 @@ const handler = async (request: Request): Promise<Response> => {
 
   console.log(ctx.measureText(title))
 
-  ctx.fillStyle = '#FCAF17';
-  ctx.fillText('$', 10, 400);
-  ctx.fillStyle = '#AAAAAA';
-  ctx.fillText('tags', 80, 400);
-  ctx.fillStyle = '#FFFFFF';
-  ctx.fillText(tags.join(' '), 50, 500);
+  // renderPrefix(ctx, 10, 400);
 
-  ctx.fillStyle = '#FCAF17';
-  ctx.fillText('$', 10, 600);
+  if(tagParam && tagParam.length > 0) {
+    ctx.fillStyle = '#AAAAAA';
+    ctx.fillText('tags', 80, 400);
+    ctx.fillStyle = '#FFFFFF';
+    ctx.fillText(tags.join(' '), 50, 500);
+  }
+
+  renderPrefix(ctx, 10, 600);
   ctx.fillStyle = '#e95295';
   ctx.fillRect(70,520,50,100);
 
