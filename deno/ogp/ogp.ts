4d5769029a66df84c31d5bb73e0950a5be04c490
swfz <sawafuji.09@gmail.com>
2023-07-27 02:36:26 +0900

    feat: とりあえず動くところまで

--- deno/ogp/ogp.ts
+++ deno/ogp/ogp.ts
@@ -1,12 +1,21 @@
 import { createCanvas } from 'https://deno.land/x/canvas/mod.ts';
+import TinySegmenter from "https://esm.sh/tiny-segmenter@0.2.0";
 
 const port = 8080;
+const textLineBase = 100;
+const statusLineBase = 80;
+const triangleBase = 40;
 
-const renderTriangle = (ctx: CanvasRenderingContext2D, x: number, y: number, color: string) => {
+const renderTriangle = (ctx: CanvasRenderingContext2D, x: number, y: number, color: string, direction: 'left'|'right') => {
+  const base = triangleBase;
   ctx.fillStyle = color;
+
+  const vertexX = direction === 'left' ? x - base : x + base;
+  const vertexY = y + base;
+
   ctx.beginPath();
-  ctx.moveTo(x + 40, y + 40);
-  ctx.lineTo(x, y + 80);
+  ctx.moveTo(vertexX, vertexY);
+  ctx.lineTo(x, y + (base * 2));
   ctx.lineTo(x, y);
   ctx.fill();
 }
@@ -16,35 +25,63 @@ const renderPrefix = (ctx: CanvasRenderingContext2D, x: number, y: number) => {
   ctx.fillText('$', x, y);
 };
 
+const selectASCII = (str: string) => {
+  const matches = str.match(/[\x00-\x7F]/g);
+
+  return matches ?? [];
+}
+
+// ASCII文字列を含めてmeasureTextを通すとASCII文字列の分だけマルチバイトで判断されている？ようなので半分にする
+const measureTextWithASCII = (ctx: CanvasRenderingContext2D, str: string) => {
+  const fullWidth = ctx.measureText(str).width;
+  const ascii = selectASCII(str);
+  const asciiWidth = ctx.measureText(ascii.join("")).width;
+
+  return fullWidth - (asciiWidth * 0.4);
+}
+
 const breakLines = (ctx: CanvasRenderingContext2D, title: string, maxWidth: number): string[] => {
-  const words = title.split('');
-  console.log(words);
+  const segmenter = new TinySegmenter();
+  const segments = segmenter.segment(title);
 
-  const processWord = (words, line='', result=[]) => {
-    if (words.length === 0) return [...result, line];
+  console.log(segments);
+  // const words = title.split('');
+  // console.log(words);
 
-    const testLine = `${line}${words[0]}`;
-    const testWidth = ctx.measureText(testLine).width;
+  const processWord = (segments, line='', result=[]) => {
+    if (segments.length === 0) return [...result, line];
+
+    const testLine = `${line}${segments[0]}`;
+    const testWidth = measureTextWithASCII(ctx, testLine);
 
     return testWidth > maxWidth
-      ? processWord(words.slice(1), `${words[0]}`, [...result, line])
-      : processWord(words.slice(1), testLine, result);
+      ? processWord(segments.slice(1), `${segments[0]}`, [...result, line])
+      : processWord(segments.slice(1), testLine, result);
   }
 
-  return processWord(words);
+  return processWord(segments);
+}
+
+const renderStatusLine = (ctx: CanvasRenderingContext2D) => {
 }
 
 const handler = async (request: Request): Promise<Response> => {
   const url = new URL(request.url);
   const title = url.searchParams.get("title") ?? 'No Title';
+  const tagsParam = url.searchParams.get("tags");
+  const tags = tagsParam ? tagsParam.split(',') : [];
 
   const [width, height] = [1200, 630];
 
   const canvas = createCanvas(width, height);
   const ctx = canvas.getContext("2d");
 
-  ctx.fillStyle = '#2f3e50';
+  ctx.fillStyle = '#313d4f';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  console.log(ctx.font);
+  ctx.font = "50pt monospace";
+  console.log('monospace:', ctx.measureText(title).width);
 
   const font = await Deno.readFile('./NotoSansCJK-Regular.ttc');
   canvas.loadFont(font, {
@@ -52,42 +89,78 @@ const handler = async (request: Request): Promise<Response> => {
   });
   ctx.font = "50pt notosans";
 
+  // StatusLine
   const terminalHeaders = ['swfz', 'til'];
-  const colors = ['#6797e8', '#a4e083']
+  const colors = ['#6797e8', '#a4e083', '#efb24a', '#ec7563'];
 
   const startPositions = terminalHeaders.reduce((acc, text) => {
-    const measured = ctx.measureText(text);
-    const x = acc.at(-1) + measured.width;
+    const measured = measureTextWithASCII(ctx, text);
+    const x = acc.at(-1) + measured;
 
     return [...acc, x];
   }, [0]);
 
   terminalHeaders.forEach((text, i) => {
     ctx.fillStyle = colors[i];
-    ctx.fillRect(startPositions[i], 30, ctx.measureText(text).width, 80);
-    ctx.fillStyle = '#EFEFEF';
-    ctx.fillText(text, startPositions[i] + 50, 90);
+    ctx.fillRect(startPositions[i], 30, measureTextWithASCII(ctx, text), 80);
+    ctx.fillStyle = '#555';
+    ctx.fillText(text, startPositions[i] + 10 + (i * 40), 90);
   });
   terminalHeaders.forEach((_, i) => {
-    renderTriangle(ctx, startPositions[i+1], 30, colors[i])
+    renderTriangle(ctx, startPositions[i+1], 30, colors[i], 'right')
   });
-  renderPrefix(ctx, 10, 200);
+  renderPrefix(ctx, 10, textLineBase * 2);
 
-  ctx.fillStyle = '#AAAAAA';
-  ctx.fillText('article --title \\', 80, 200);
+  // Command
+  ctx.fillStyle = '#888';
+  ctx.fillText('article --title \\', 80, textLineBase * 2);
 
+  console.log('title:', ctx.measureText(title).width);
+
+  // Title
   const titleLines = breakLines(ctx, title, 1150);
   ctx.fillStyle = '#FFFFFF';
   titleLines.forEach((line, i) => {
-    ctx.fillText(line, 50, 300 + i*100)
+    ctx.fillText(line, 50, 300 + i*textLineBase)
   });
 
-  const lastLineWidth = ctx.measureText(titleLines.at(-1)).width;
+  // Cursor
+  const lastLineWidth = measureTextWithASCII(ctx, titleLines.at(-1));
   const cursorX = 50 + lastLineWidth + 10;
   const cursorY = 200 + 30 + (titleLines.length -1) * 100;
 
   ctx.fillStyle = '#ec80f7';
   ctx.fillRect(cursorX, cursorY, 50, 100);
+
+  // タイトル表示を優先させるため、タイトルが3行までの場合はTagも表示させる
+  if (tags.length > 0 && titleLines.length <= 3) {
+    // Tags
+    ctx.fillStyle = '#333';
+    ctx.fillRect(0, 540, 1200, 80);
+    ctx.fillStyle = '#999';
+    ctx.fillText('Tags', 10, 600);
+    // renderTriangle(ctx, 600, 540, '#999', 'left');
+
+    const tagsStartPositions = tags.reverse().reduce((acc, tag, i) => {
+      const measured = measureTextWithASCII(ctx, tag);
+      const x = acc.at(-1) - measured - (triangleBase * i);
+
+      return [...acc, x];
+    }, [1200]).reverse();
+
+    console.log(tagsStartPositions);
+
+    const tagColors = colors;
+    tags.reverse().forEach((tag, i) => {
+      ctx.fillStyle = tagColors[i];
+      ctx.fillRect(tagsStartPositions[i], 540, measureTextWithASCII(ctx, tag) + (triangleBase * (tags.length - i)), 80);
+      ctx.fillStyle = '#555';
+      ctx.fillText(tag, tagsStartPositions[i], 600);
+    });
+    tags.reverse().forEach((_, i) => {
+      renderTriangle(ctx, tagsStartPositions[i], 540, tagColors[i], 'left')
+    });
+  }
 
   const headers = new Headers();
   headers.set("content-type", "image/png");
