756d5abc7e5b21de2607179c89747557dc1bba50
swfz <sawafuji.09@gmail.com>
2023-08-10 02:59:31 +0900

    chore: format

--- javascript/notion-to-obsidian-daily-note/habits.js
+++ javascript/notion-to-obsidian-daily-note/habits.js
@@ -1,140 +1,152 @@
-import {Client, LogLevel } from "@notionhq/client";
-import {fromMarkdown} from 'mdast-util-from-markdown'
-import {frontmatter} from 'micromark-extension-frontmatter'
-import {toMarkdown} from 'mdast-util-to-markdown'
-import {frontmatterFromMarkdown, frontmatterToMarkdown} from 'mdast-util-frontmatter'
-import * as fs from 'fs'
-
-// 前提
-// Notionに日毎で習慣化のプロパティが存在するページがあり、それを集約したDatabaseがある
-// DatabaseIDを環境変数に設定しておく
-// Databaseに存在する各日にちの習慣化リストを取得しObsidianのDailyNoteにコンテンツとして追加する
-// ObsidianのDailyNoteはYYYY-MM-DD.mdというファイル名で存在する
-// DailyNoteが存在するディレクトリはコマンドライン引数で渡す
-// 過去データの入れ込みで一度きりの実行を想定しているため冪等性などは考慮していない
+import { Client, LogLevel } from "@notionhq/client";
+import { fromMarkdown } from "mdast-util-from-markdown";
+import { frontmatter } from "micromark-extension-frontmatter";
+import { toMarkdown } from "mdast-util-to-markdown";
+import {
+  frontmatterFromMarkdown,
+  frontmatterToMarkdown,
+} from "mdast-util-frontmatter";
+import * as fs from "fs";
 
 const getHabitsFromPage = async (notion, page) => {
-  const pageId = page.id
-  const response = await notion.pages.retrieve({ page_id: pageId })
+  const pageId = page.id;
+  const response = await notion.pages.retrieve({ page_id: pageId });
 
   // console.log(response);
   const habitKeys = Object.keys(response.properties);
 
-  const properties = habitKeys.map((key) => {
-    const property = response.properties[key];
-    const propType = property.type;
+  const properties = habitKeys
+    .map((key) => {
+      const property = response.properties[key];
+      const propType = property.type;
 
-    if (['title', 'relation', 'formula', 'select'].includes(propType)) {
-      return null;
-    }
+      if (["title", "relation", "formula", "select"].includes(propType)) {
+        return null;
+      }
 
-    return {
-      name: key,
-      value: property[propType],
-    }
-  }).filter(p => p);
+      return {
+        name: key,
+        value: property[propType],
+      };
+    })
+    .filter((p) => p);
 
   return properties;
-}
+};
 
 const markdownToAst = (filename) => {
   const markdown = fs.readFileSync(filename);
 
   return fromMarkdown(markdown, {
-    extensions: [frontmatter(['yaml'])],
-    mdastExtensions: [frontmatterFromMarkdown(['yaml'])]
+    extensions: [frontmatter(["yaml"])],
+    mdastExtensions: [frontmatterFromMarkdown(["yaml"])],
   });
-}
+};
 
 const createHabitsAst = (habits) => {
-  const items = habits.map(habit => ({
-    type: 'listItem',
+  const items = habits.map((habit) => ({
+    type: "listItem",
     children: [
       {
-        type: 'paragraph',
+        type: "paragraph",
         children: [
           {
-            type: 'text',
-            value: `[${habit.value ? 'x' : ' '}] ${habit.name}`
-          }
-        ]
-      }
-    ]
+            type: "text",
+            value: `[${habit.value ? "x" : " "}] ${habit.name}`,
+          },
+        ],
+      },
+    ],
   }));
 
   const list = {
-    type: 'list',
+    type: "list",
     start: null,
     spread: false,
-    children: items
-  }
+    children: items,
+  };
 
   return list;
-}
+};
 
 const createHabitsHeadingAst = () => {
   return {
-    type: 'heading',
+    type: "heading",
     depth: 2,
     children: [
       {
-        type: 'text',
-        value: 'Habits'
-      }
-    ]
-  }
-}
+        type: "text",
+        value: "Habits",
+      },
+    ],
+  };
+};
 
 const mergeDailyNote = async (directory, properties) => {
-  const date = properties.find((p) => p.name === 'date').value.start;
+  const date = properties.find((p) => p.name === "date").value.start;
   const obsidianDailyNoteFilename = `${directory}/${date}.md`;
 
   const existMarkdown = fs.existsSync(obsidianDailyNoteFilename);
   if (!existMarkdown) {
-    fs.writeFileSync(obsidianDailyNoteFilename, '');
+    fs.writeFileSync(obsidianDailyNoteFilename, "");
   }
 
-  const habits = properties.filter((p) => p.name !== 'date');
+  const habits = properties.filter((p) => p.name !== "date");
   const ast = markdownToAst(obsidianDailyNoteFilename);
 
   const habitsAst = createHabitsAst(habits);
   const habitsHeadingAst = createHabitsHeadingAst();
 
-  const existHabits = ast.children.find((item) => item.type === 'heading' && item.children[0]?.value === 'Habits');
+  const existHabits = ast.children.find(
+    (item) => item.type === "heading" && item.children[0]?.value === "Habits",
+  );
 
   // すでに作成済みの場合は何もしない
   if (existHabits) {
     return;
   }
 
-  const existMemoHeading = ast.children.find((item) => item.type === 'heading' && item.children[0]?.value === 'Memo');
+  const existMemoHeading = ast.children.find(
+    (item) => item.type === "heading" && item.children[0]?.value === "Memo",
+  );
 
   const insertBeforeMemo = (acc, item, i, array) => {
-    if (array[i+1]?.type === 'heading' && array[i+1].children[0]?.value === 'Memo') {
+    if (
+      array[i + 1]?.type === "heading" &&
+      array[i + 1].children[0]?.value === "Memo"
+    ) {
       return [...acc, item, habitsHeadingAst, habitsAst];
     }
 
     return [...acc, item];
-  }
+  };
 
-  const children = existMemoHeading ? ast.children.reduce(insertBeforeMemo, []) : [...ast.children, habitsHeadingAst ,habitsAst];
+  const children = existMemoHeading
+    ? ast.children.reduce(insertBeforeMemo, [])
+    : [...ast.children, habitsHeadingAst, habitsAst];
 
-  const afterAst = {...ast, ...{children}}
+  const afterAst = { ...ast, ...{ children } };
   // console.dir(afterAst, {depth: null});
 
   const options = {
-    bullet: '-',
-    extensions: [frontmatterToMarkdown(['yaml'])]
+    bullet: "-",
+    extensions: [frontmatterToMarkdown(["yaml"])],
   };
 
   // FIXME: unsafeオプションを使ってエスケープさせないようにしたかったが、うまくいかなかったため場当たり的な対応をしている
   const replacer = (str) => {
-    return str.replace(/\\\[/g, '[').replace(/\\_/g, '_').replace(/\\&/g, '&').replace(/\\\*/g, '*');
-    // return str.replace(/\\\[/g, '[').replace(/\\_/g, '_');
-  }
+    return str
+      .replace(/\\\[/g, "[")
+      .replace(/\\_/g, "_")
+      .replace(/\\&/g, "&")
+      .replace(/\\\*/g, "*");
+  };
 
-  fs.writeFileSync(obsidianDailyNoteFilename, replacer(toMarkdown(afterAst, options)));
-}
+  fs.writeFileSync(
+    obsidianDailyNoteFilename,
+    replacer(toMarkdown(afterAst, options)),
+  );
+};
 
 const main = async () => {
   const args = process.argv.slice(2);
@@ -145,7 +157,7 @@ const main = async () => {
     logLevel: LogLevel.DEBUG,
   });
 
-  const databaseId = process.env.NOTION_DATABASE_ID || '';
+  const databaseId = process.env.NOTION_DATABASE_ID || "";
   const obsidianDailyNoteDir = args[0];
 
   const response = await notion.databases.query({
@@ -160,10 +172,12 @@ const main = async () => {
   // await mergeDailyNote(obsidianDailyNoteDir, properties);
   // ==== 1件で試す
 
-  Promise.all(pages.map(async (page) => {
-    const properties = await getHabitsFromPage(notion, page);
-    await mergeDailyNote(obsidianDailyNoteDir, properties);
-  }));
-}
+  Promise.all(
+    pages.map(async (page) => {
+      const properties = await getHabitsFromPage(notion, page);
+      await mergeDailyNote(obsidianDailyNoteDir, properties);
+    }),
+  );
+};
 
-main()
+main();
